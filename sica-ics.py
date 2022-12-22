2a7419e7eaaaf08c4ab2ab111c121b29d36e5241
Christian Glodt <chris@mind.lu>
2022-12-22 10:42:37 +0100

    print http headers

--- sica-ics.py
+++ sica-ics.py
@@ -7,7 +7,9 @@ import json
 import datetime
 import icalendar
 
+
 COMMUNE_ID = os.environ.get('COMMUNE_ID', '24')
+
 
 def get_pickups():
     response = requests.get('https://sicaapp.lu', cookies={'commune_id': COMMUNE_ID})
@@ -28,7 +30,8 @@ def get_pickups():
                 pickups = [pt['name'] for pt in entry['pickupTypes']]
                 yield date, pickups
 
-def main():
+
+def get_ical():
     cal = icalendar.Calendar()
     cal.add('prodid', 'sica-ics.py')
     cal.add('version', '2.0')
@@ -41,7 +44,18 @@ def main():
             event.add('dtend', date + datetime.timedelta(days=1))
             cal.add_component(event)
     
-    print(cal.to_ical())
+    return cal.to_ical().decode('utf-8')
+
+
+def main():
+    print('HTTP/1.1 200 OK')
+    print('Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0')
+    print('Content-Type: text/calendar')
+    print('Pragma: no-cache')
+    print('Expires: -1')
+    print('Access-Control-Allow-Origin: *')
+    print('')
+    print(get_ical())
 
 
 if __name__ == '__main__':

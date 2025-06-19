ecf879c08d6ac742f5e699b730660f9fe56c03be
Christian Glodt <chris@mind.lu>
2025-06-19 13:26:18 +0200

    Adapt to new sica.lu api

--- sica-ics.py
+++ sica-ics.py
@@ -8,7 +8,7 @@ import datetime
 import icalendar
 
 
-COMMUNE_ID = os.environ.get('COMMUNE_ID', '24')
+COMMUNE_ID = os.environ.get('COMMUNE_ID', '5')
 EVENT_START_HOUR = int(os.environ.get('START_HOUR', '6'))
 EVENT_START_MINUTE = int(os.environ.get('START_MINUTE', '0'))
 EVENT_END_HOUR = int(os.environ.get('END_HOUR', '10'))
@@ -16,26 +16,19 @@ EVENT_END_MINUTE = int(os.environ.get('END_MINUTE', '0'))
 
 
 def get_pickups():
-    response = requests.get('https://sicaapp.lu', cookies={'commune_id': COMMUNE_ID})
-    html = response.text
+    from_date = (datetime.datetime.now() - datetime.timedelta(days=31)).date().isoformat()
+    to_date = (datetime.datetime.now() + datetime.timedelta(days=90)).date().isoformat()
+    response = requests.get(f'https://dashboard.sicaapp.lu/api/api/app/pickup-date?filter[community_id]={COMMUNE_ID}&filter[dateRange]={from_date},{to_date}')
 
-    escaped_json_match = re.search(r'obj = JSON.parse\("(.*)"\);', html)
-    if not escaped_json_match:
-        raise Exception('Calendar data not found in HTML')
+    data = response.json()
 
-    escaped_json = escaped_json_match.group(1)
-    json_txt = escaped_json.replace('\\"', '"')
-    while json_txt.startswith('\\r') or json_txt.startswith('\\n'):
-        json_txt = json_txt[2:]
-    data = json.loads(json_txt)
+    for entry in data:
+        if entry['isDisabled']:
+            continue
 
-    for o in data:
-        schedule = o['schedule']
-        for entry in schedule:
-            if entry['pickupTypes']:
-                dt = datetime.datetime.strptime(entry['date'], '%Y%m%d')
-                pickups = [pt['name'] for pt in entry['pickupTypes']]
-                yield dt, pickups
+        dt = datetime.datetime.strptime(entry['date'], '%Y-%m-%d %H:%M:%S')
+        pickup = [entry['pickup_type']['name']['en']]
+        yield dt, pickup
 
 
 def get_ical():
@@ -43,13 +36,12 @@ def get_ical():
     cal.add('prodid', 'sica-ics.py')
     cal.add('version', '2.0')
 
-    for dt, pickups in get_pickups():
-        for pickup in pickups:
-            event = icalendar.Event()
-            event.add('summary', pickup)
-            event.add('dtstart', dt + datetime.timedelta(hours=EVENT_START_HOUR, minutes=EVENT_START_MINUTE))
-            event.add('dtend', dt + datetime.timedelta(hours=EVENT_END_HOUR, minutes=EVENT_END_MINUTE))
-            cal.add_component(event)
+    for dt, pickup in get_pickups():
+        event = icalendar.Event()
+        event.add('summary', pickup)
+        event.add('dtstart', dt.replace(hour=0, minute=0, second=0) + datetime.timedelta(hours=EVENT_START_HOUR, minutes=EVENT_START_MINUTE))
+        event.add('dtend', dt.replace(hour=0, minute=0, second=0) + datetime.timedelta(hours=EVENT_END_HOUR, minutes=EVENT_END_MINUTE))
+        cal.add_component(event)
     
     return cal.to_ical().decode('utf-8')
 
